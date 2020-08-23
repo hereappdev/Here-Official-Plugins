6478a5684321cd36657ecf6e484f4afa62938cb2
Here App Dev <HereDotApp@gmail.com>
2020-08-23 22:12:01 +0800

    refactor(apple-dev): adopted to the new API

--- source/Apple-Developer-Releases/index.js
+++ source/Apple-Developer-Releases/index.js
@@ -1,106 +1,80 @@
-const _ = require("underscore")
-const net = require("net")
+const _ = require("underscore");
+const net = require("net");
 
 function updateData() {
-    const LIMIT = 50
-    
-    here.miniWindow.set({ title: "Updating…" })
+    const LIMIT = 50;
+
+    here.miniWindow.data.title = "Updating…";
+    here.miniWindow.reload();
+
     here.parseRSSFeed("https://developer.apple.com/news/releases/rss/releases.rss")
-    .then((feed) => {
-    	// console.log(JSON.stringify(feed))
-        if (feed.items.length <= 0) {
-            return here.miniWindow.set({ title: "No item found." })
-        }
-    
-        if (feed.items.length > LIMIT) {
-            feed.items = feed.items.slice(0, LIMIT)
-        }
-    
-        const topFeed = feed.items[0]
-        // Mini Window
-        here.miniWindow.set({
-            onClick: () => { if (topFeed.link != undefined)  { here.openURL("https://developer.apple.com/news/releases/") } },
-            title: topFeed.title,
-            detail: "Apple Developer Releases",
-            accessory: { badge: feed.items.length.toString() }
-        })
-
-        let dataAll = feed.items.map( function(item, index){
-            return {
-                title: item.title,
-                // accessory: {
-                //     title: "date"
-                // },
-                onClick: () => { if (item.link != undefined)  { here.openURL(item.link) } },
+        .then((feed) => {
+            // console.log(JSON.stringify(feed))
+            if (feed.items.length <= 0) {
+                return here.miniWindow.set({ title: "No item found." });
             }
-        })
 
-        let dataBeta = feed.items.filter(function(item, index) {
-            if(item.title.indexOf("beta") > 1){
-                // console.log(index)
-                return true;  
+            if (feed.items.length > LIMIT) {
+                feed.items = feed.items.slice(0, LIMIT);
             }
-            return false
+
+            const topFeed = feed.items[0];
+
+            // Mini Window
+            here.miniWindow.data = {
+                title: topFeed.title,
+                detail: "Apple Developer Releases",
+                accessory: { badge: feed.items.length.toString() },
+            };
+            here.miniWindow.onClick(() => {
+                here.openURL("https://developer.apple.com/news/releases/");
+            });
+            here.miniWindow.reload();
+
+            // Popover
+            let dataAll = feed.items.map((item) => {
+                return {
+                    title: item.title.trim(),
+                    onClick: () => {
+                        if (item.link != undefined) {
+                            here.openURL(item.link);
+                        }
+                    },
+                };
+            });
+
+            let dataBeta = dataAll.filter((item) => {
+                return item.title.indexOf("beta") != -1;
+            });
+
+            let dataReleased = dataAll.filter((item) => {
+                return item.title.indexOf("beta") == -1;
+            });
+
+            let tabs = [
+                { title: "All", data: dataAll },
+                { title: "Beta", data: dataBeta },
+                { title: "Released", data: dataReleased },
+            ];
             
-        }).map( function(item, index){
-            return {
-                title: item.title,
-                // accessory: {
-                //     title: "date"
-                // },
-                onClick: () => { if (item.link != undefined)  { here.openURL(item.link) } },
-            }
+            here.popover = new here.TabPopover();
+            here.popover.data = tabs;
+            here.popover.reload();
         })
-
-        let dataReleased = feed.items.filter(function(item, index) {
-            if(item.title.indexOf("beta") < 1){
-                // console.log(index)
-                return true;  
-            }
-            return false
-        }).map( function(item, index){
-            return {
-                title: item.title,
-                // accessory: {
-                //     title: "date"
-                // },
-                onClick: () => { if (item.link != undefined)  { here.openURL(item.link) } },
-            }
-        })
-
-        let tabs = [
-            {
-                title: "All",
-                data: dataAll
-            },
-            {
-                title: "Beta",
-                data: dataBeta
-            },
-            {
-                title: "Released",
-                data: dataReleased
-            }
-        ]
-        here.popover.set(tabs)
-
-
-    })
-    .catch((error) => {
-        console.error(`Error: ${JSON.stringify(error)}`)
-    })
-
+        .catch((error) => {
+            console.error(`Error: ${JSON.stringify(error)}`);
+        });
 }
 
-here.on('load', () => {
-    updateData()
+here.on("load", () => {
+    updateData();
     // Update every 2 hours
-    setInterval(updateData, 2*3600*1000);
-})
+    setInterval(updateData, 2 * 3600 * 1000);
+});
 
 net.onChange((type) => {
-    console.verbose("Connection type changed:", type)
+    console.verbose("Connection type changed:", type);
     if (net.isReachable()) {
-        updateData()
+        updateData();
     }
-})
+});
