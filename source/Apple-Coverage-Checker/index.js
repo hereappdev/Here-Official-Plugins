fbe2eda5e430287340f37db045a2fca4320a4647
Here App Dev <HereDotApp@gmail.com>
2020-08-23 22:12:01 +0800

    refactor(apple-coverage): adopted to the new API

--- source/Apple-Coverage-Checker/index.js
+++ source/Apple-Coverage-Checker/index.js
@@ -1,15 +1,18 @@
-here.on('load', () => {
-    here.setMiniWindow({ title: "Apple Service Coverage", detail: "Check the Status of your Apple Devices" })
-    
-    here.setPopover({
-        type: "webView",
-        data: {
-            url: "https://checkcoverage.apple.com",
-            width: 375,
-            height: 580,
-            backgroundColor: "#ffffff",
-            foregroundColor: rgba(0, 0, 0, 0.5),
-            hideStatusBar: false
-        }
-    })
-})
+here.on("load", () => {
+    here.miniWindow.data = {
+        title: "Apple Service Coverage",
+        detail: "Check the Status of your Apple Devices",
+    };
+    here.miniWindow.reload();
+
+    here.popover = new here.WebViewPopover()
+    here.popover.data = {
+        url: "https://checkcoverage.apple.com",
+        width: 375,
+        height: 580,
+        backgroundColor: "#ffffff",
+        foregroundColor: rgba(0, 0, 0, 0.5),
+        hideStatusBar: false,
+    };
+    here.popover.reload();
+});
