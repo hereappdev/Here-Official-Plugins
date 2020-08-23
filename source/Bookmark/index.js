a7e6b720285ce136408a2f9f0f9a2261434ecbb3
Here App Dev <HereDotApp@gmail.com>
2020-08-23 22:12:01 +0800

    refactor(bookmark): updated

--- source/Bookmark/index.js
+++ source/Bookmark/index.js
@@ -1,26 +1,25 @@
-const pref = require("pref")
-
-var json = pref.all()
+const pref = require("pref");
+const json = pref.all();
 
 if (json == undefined) {
-    console.log("No prefs found.")
+    console.log("No prefs found.");
 }
 
-here.on('load', () => {
-    here.setMiniWindow({ 
+here.on("load", () => {
+    here.miniWindow.data = {
         title: json["websiteName"],
-        detail: json["websiteURL"]
-    })
-console.log(json["windowWidth"])
-    here.setPopover({
-        type: "webView",
-        data: {
-            url: json["websiteURL"],
-            width: Number(json["windowWidth"]),
-            height: Number(json["windowHeight"]),
-            backgroundColor: json["backgroundColor"],
-            foregroundColor: json["foregroundColor"],
-            hideStatusBar: json["hideStatusBar"] == "true" ? 1 : 0
-        }
-    })
-})
+        detail: json["websiteURL"],
+    };
+    here.miniWindow.reload();
+
+    here.popover = new here.WebViewPopover();
+    here.popover.data = {
+        url: json["websiteURL"],
+        width: json["windowWidth"],
+        height: json["windowHeight"],
+        backgroundColor: json["backgroundColor"],
+        foregroundColor: json["foregroundColor"],
+        hideStatusBar: json["hideStatusBar"] == "true" ? 1 : 0,
+    };
+    here.popover.reload();
+});
