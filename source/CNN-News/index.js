08c856fd586ee818fda0b528e95a35754a2871b3
Here App Dev <HereDotApp@gmail.com>
2020-08-23 22:12:01 +0800

    refactor(cnn): adopted to the new API

--- source/CNN-News/index.js
+++ source/CNN-News/index.js
@@ -1,53 +1,65 @@
-const _ = require("underscore")
-const net = require("net")
+const _ = require("underscore");
+const net = require("net");
 
 function updateData() {
-    const LIMIT = 10
+    const LIMIT = 10;
 
-    here.miniWindow.set({ title: "Updating…" })
+    here.miniWindow.data.title = "Updating…";
+    here.miniWindow.reload();
 
     // API: http://rss.cnn.com/rss/cnn_topstories.rss
     // API Speedy: https://apispeedy.com/cnn/
 
     here.parseRSSFeed("https://apispeedy.com/cnn/")
-    .then((feed) => {
-        if (feed.items.length <= 0) {
-            return here.miniWindow.set({ title: "No item found." })
-        }
-
-        if (feed.items.length > LIMIT) {
-            feed.items = feed.items.slice(0, LIMIT)
-        }
-
-        const topFeed = feed.items[0]
-
-        // Mini Window
-        here.miniWindow.set({
-            onClick: () => { if (topFeed.link != undefined)  { here.openURL(topFeed.link) } },
-            title: topFeed.title,
-            detail: "CNN News"
-        })
-        here.popover.set(_.map(feed.items, (item, index) => {
-            return {
-                title: item.title,
-                onClick: () => { if (item.link != undefined)  { here.openURL(item.link) } },
+        .then((feed) => {
+            if (feed.items.length <= 0) {
+                console.error("No item found.");
+                return;
             }
-        }))
-    })
-    .catch((error) => {
-        console.error(`Error: ${JSON.stringify(error)}`)
-    })
+
+            if (feed.items.length > LIMIT) {
+                feed.items = feed.items.slice(0, LIMIT);
+            }
+
+            const topFeed = feed.items[0];
+
+            // Mini Window
+            here.miniWindow.data = {
+                title: topFeed.title,
+                detail: "CNN News",
+            };
+            here.miniWindow.onClick(() => {
+                here.openURL(topFeed.link);
+            });
+            here.miniWindow.reload();
+
+            // Popover
+            here.popover.data = _.map(feed.items, (item, index) => {
+                return {
+                    title: item.title,
+                    onClick: () => {
+                        if (item.link != undefined) {
+                            here.openURL(item.link);
+                        }
+                    },
+                };
+            });
+            here.popover.reload()
+        })
+        .catch((error) => {
+            console.error(`Error: ${JSON.stringify(error)}`);
+        });
 }
 
-here.on('load', () => {
-    updateData()
+here.on("load", () => {
+    updateData();
     // Update every 2 hours
-    setInterval(updateData, 2*3600*1000);
-})
+    setInterval(updateData, 2 * 3600 * 1000);
+});
 
 net.onChange((type) => {
-    console.verbose("Connection type changed:", type)
+    console.verbose("Connection type changed:", type);
     if (net.isReachable()) {
-        updateData()
+        updateData();
     }
-})
+});
