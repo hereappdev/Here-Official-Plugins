a7d2e852b7d4b2170246e81395a997c1fbd176ec
Here App Dev <HereDotApp@gmail.com>
2020-08-23 22:12:01 +0800

    refactor(goldprice): adopted to the new API

--- source/Gold-Price/index.js
+++ source/Gold-Price/index.js
@@ -1,73 +1,73 @@
-const _ = require("underscore")
-const http = require("http")
-const net = require("net")
-    
-function strToVar(str) {
-    var json = (new Function("return " + str))();
-    return json;
-}
+const _ = require("underscore");
+const http = require("http");
+const net = require("net");
 
 function updateData() {
-    here.miniWindow.set({ title: "Updating…" })
+    here.miniWindow.set({ title: "Updating…" });
     http.get("https://hq.sinajs.cn/?list=gds_AUTD")
-    .then(function(response) {
-        let goldPrice
-        let goldDate
-    
-        let data = response.data
-        if (data != undefined) {
-            goldPrice = data.toString().split("=\"")[1].split(",")[0]
-            goldDate = data.toString().split("=\"")[1].split(",")[12] + " " + data.toString().split("=\"")[1].split(",")[6]
-        }
-    
-        // console.log(goldPrice)
-    
-        // Menu Bar
-        here.menuBar.set({
-            title: goldPrice,
-            detail: "Gold"
-        })
-    
-        // Mini Window
-        here.miniWindow.set({
-            title: "黄金价格",
-            detail: goldDate,
-            accessory: {
-                title: goldPrice
+        .then(function (response) {
+            let goldPrice;
+            let goldDate;
+
+            let data = response.data;
+            if (data != undefined) {
+                //  var hq_str_gds_AUTD="417.70,0,417.52,417.70,423.29,411.33,15:30:07,417.55,417.59,109956,0.00,0.00,2020-08-28,黄金延期";
+                data = data.split(",");
+                const firstItem = data[0].split('"');
+                goldPrice = firstItem[firstItem.length - 1];
+                goldDate = data[data.length - 2];
             }
+
+            // console.log(goldPrice)
+
+            // Menu Bar
+            here.menuBar.data = {
+                title: goldPrice,
+                detail: "Gold",
+            };
+            here.menuBar.reload();
+
+            // Mini Window
+            here.miniWindow.data = {
+                title: "黄金价格",
+                detail: goldDate,
+                accessory: {
+                    title: goldPrice,
+                },
+            };
+            here.miniWindow.reload();
+
+            // Dock
+            here.dock.data = {
+                title: goldPrice,
+                detail: "金价",
+            };
+            here.dock.reload();
         })
-    
-        // Dock
-        here.dock.set({
-            title: goldPrice,
-            detail: "金价"
-        })
-    })
-    .catch(function(error) {
-        console.error(`Error: ${JSON.stringify(error)}`)
-        here.miniWindow.set({ title: JSON.stringify(error) })
-    })
+        .catch(function (error) {
+            console.error(`Error: ${JSON.stringify(error)}`);
+            here.miniWindow.set({ title: JSON.stringify(error) });
+        });
 }
 
-here.on('load', () => {
-    updateData()
+here.on("load", () => {
+    updateData();
     // Update every 2 hours
-    setInterval(updateData, 2*3600*1000);
-})
-
+    setInterval(updateData, 2 * 3600 * 1000);
+});
 
 net.onChange((type) => {
-    console.verbose("Connection type changed:", type)
+    console.verbose("Connection type changed:", type);
     if (net.isReachable()) {
-        updateData()
+        updateData();
     }
-})
+});
 
-here.setPopover({
-    type: "webView",
-    data: {
-        url: "http://gu.sina.cn/m/?vt=4&cid=76613#/futures/month?symbol=AU0",
-        width: 375,
-        height: 500
-    }
-})
+// WebView
+here.popover = new here.WebViewPopover();
+here.popover.data = {
+    url: "http://gu.sina.cn/m/?vt=4&cid=76613#/futures/month?symbol=AU0",
+    width: 375,
+    height: 500,
+};
+here.popover.reload();
