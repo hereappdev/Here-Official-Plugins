8fb028373b0aa4a1e1573605454c8ed7e9257193
Here App Dev <HereDotApp@gmail.com>
2020-08-27 16:41:36 +0800

    add new plugin YuTang

---
+++ source/YuTang/README.md
@@ -0,0 +1,12 @@
+# 鱼塘热门
+
+> 显示鱼塘热门推荐和Top榜
+
+## Development
+
+* Thanks to [mo.fish](https://mo.fish/) for API support
+* API: [https://www.tophub.fun:8888/v2/GetAllInfoGzip?id=1065](https://www.tophub.fun:8888/v2/GetAllInfoGzip?id=1065)
+
+## License
+
+This is an open source plugin published under the MIT License
---
+++ source/YuTang/index.js
@@ -0,0 +1,128 @@
+const _ = require("underscore")
+const http = require("http")
+const net = require("net")
+const pref = require("pref")
+
+// const jsonPref = pref.all()
+
+function getData(api) {
+
+    const LIMIT = 30
+
+    let entryList = []
+    return http.get(api)
+    .then(function(response) {
+        const json = response.data.Data.data
+        if (json == undefined) {
+            console.log(222)
+            json = response.data.Data.data.data
+        }
+
+
+        if (json == undefined) {
+            return here.miniWindow.set({ title: "Invalid data." })
+        }
+    
+        let entryList = json
+
+        if (entryList.length <= 1) {
+            return here.miniWindow.set({ title: "Entrylist is empty." })
+        }
+    
+        if (entryList.length > LIMIT) {
+            entryList = entryList.slice(0, LIMIT)
+        }
+            return entryList
+        })
+}
+
+function updateData() {
+
+    here.miniWindow.set({ title: "Updating…" })
+
+    Promise.all([
+        getData("https://www.tophub.fun:8888/v2/GetAllInfoGzip?id=1006"),
+        getData("https://www.tophub.fun:8888/v2/GetAllInfoGzip?id=1065")
+    ]).then(function (values) {
+        console.log(values)
+
+        const topFeed = values[0][0]
+        
+        // Mini Window
+        here.miniWindow.set({
+            onClick: () => { here.openURL(topFeed.Url) },
+            title: topFeed.Title,
+            detail: "鱼塘热榜"
+        })
+
+        here.menuBar.set({
+            title: ""
+        })
+
+        let popovers = []
+
+
+        values.forEach(function(element, index){
+            // console.log(index)
+            // console.log(values[index])
+
+            popovers[index] = _.map(values[index], (feed, index) => {
+                return {
+                    title: feed.Title,
+                    accessory: {
+                        title: feed.type
+                    },
+                    // detail: feed.description,
+                    onClick: () => { here.openURL(feed.Url) }
+                }
+            })
+
+        
+
+            // popovers[index].push({
+            //     title: "View All…",
+            //     onClick: () => { _.each(values[index], (feed) => { here.openURL(feed.url) }) }
+            // })
+        });
+
+
+        let tabs = [
+            {
+                title: "鱼塘TOP榜",
+                data: popovers[0]
+            },
+            {
+                title: "鱼塘推荐流",
+                data: popovers[1]
+            },
+            // {
+            //     title: "创业",
+            //     data: popovers[2]
+            // },
+            // {
+            //     title: "生活",
+            //     data: popovers[3]
+            // },
+            // {
+            //     title: "政务",
+            //     data: popovers[4]
+            // }
+        ]
+
+        here.popover.set(tabs)
+
+    });
+}
+
+here.on('load', () => {
+    updateData()
+    // Update every 2 hours
+    setInterval(updateData, 12 * 3600 * 1000)
+})
+
+net.on('change', (type) => {
+    console.log("Connection type changed:", type)
+    if (net.isReachable()) {
+        updateData()
+    }
+})
