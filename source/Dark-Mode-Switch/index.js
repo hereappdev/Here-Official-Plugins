c38b91c4a0bf50f983444c94fb6e09ff380e96ad
Here App Dev <HereDotApp@gmail.com>
2021-02-26 00:15:14 +0800

    test here.menuBar.setIcon("./sun.png")

--- source/Dark-Mode-Switch/index.js
+++ source/Dark-Mode-Switch/index.js
@@ -16,6 +16,48 @@ here.on("load", () => {
         title: "Dark Mode Switch",
         detail: "Click to switch",
     };
+
+    // Menu Bar
+    here.menuBar = new MenuBar();
+    here.menuBar.onClick(() => {
+
+        if (!here.miniWindow.data.accessory.isOn) {
+                here.exec(
+                    `osascript -e 'tell app "System Events" to tell appearance preferences to set dark mode to true'`
+                ).then(() => {
+                    console.log("Switch to dark.");
+                    here.menuBar.set({
+                        title: "Dark"
+                    })
+                    // here.miniWindow.data.accessory.isOn = false;
+                    // console.log(`after isOn: ${isOn}`);
+                    here.menuBar.setIcon("./sun.png")
+                    .catch((err) => {
+                         console.error(err)
+                    })
+                    here.menuBar.reload();
+                });
+            } else {
+                here.exec(
+                    `osascript -e 'tell app "System Events" to tell appearance preferences to set dark mode to false'`
+                ).then(() => {
+                    console.log("Switch to light.");
+                    //  here.miniWindow.data.accessory.isOn = true;
+                    // console.log(`after isOn: ${isOn}`);
+                    here.menuBar.set({
+                        title: "Light"
+                    })
+                    here.menuBar.setIcon("./menuBarIcon.png")
+                    .catch((err) => {
+                         console.error(err)
+                    })
+                    here.menuBar.reload();
+                });
+            }
+
+    });
+
+    here.menuBar.reload();
     
     here.miniWindow.data.accessory = new here.SwitchAccessory({
         isOn: false,
@@ -26,7 +68,7 @@ here.on("load", () => {
                     `osascript -e 'tell app "System Events" to tell appearance preferences to set dark mode to true'`
                 ).then(() => {
                     // console.log("Switch to dark.");
-//                     here.miniWindow.data.accessory.isOn = false;
+                    // here.miniWindow.data.accessory.isOn = false;
                     // console.log(`after isOn: ${isOn}`);
                 });
             } else {
@@ -34,7 +76,7 @@ here.on("load", () => {
                     `osascript -e 'tell app "System Events" to tell appearance preferences to set dark mode to false'`
                 ).then(() => {
                     // console.log("Switch to light.");
-//                     here.miniWindow.data.accessory.isOn = true;
+                    // here.miniWindow.data.accessory.isOn = true;
                     // console.log(`after isOn: ${isOn}`);
                 });
             }
