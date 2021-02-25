b9b6538c7fdd771da53b5ae12d4b3bc7c6d66493
Here App Dev <HereDotApp@gmail.com>
2021-02-25 14:46:08 +0800

    refactor(empty-trash): support for here.menuBar.onClick

--- source/Empty-Trash/index.js
+++ source/Empty-Trash/index.js
@@ -1,3 +1,32 @@
+function onClick() {
+    here.exec(`ls ~/.Trash | wc -l`)
+        .then((stdOut) => {
+            const count = parseInt(stdOut);
+            console.log(`Files count: ${count}`);
+            if (typeof count == "number" && count == 0) {
+                console.log(`The trash can is empty.`);
+                return Promise.resolve();
+            } else {
+                return here.exec("osascript -e 'tell application \"Finder\" to empty trash'");
+            }
+        })
+        .then((stdOut) => {
+            // console.log("stdOut: ", stdOut)
+            if (stdOut != undefined && stdOut.length > 0) {
+                // Interactive command
+                // Treat it as an Error for now
+                console.error(`Error: ${stdOut}`);
+                here.hudNotification("Failed to empty trash");
+            } else {
+                here.hudNotification("Files deleted");
+            }
+        })
+        .catch((error) => {
+            console.error(`Faild to execute command. Error: ${JSON.stringify(error)}`);
+            here.hudNotification("Failed to empty trash");
+        });
+}
+
 here.on("load", () => {
     // Mini Window
     here.miniWindow.data = {
@@ -6,32 +35,18 @@ here.on("load", () => {
     };
     here.miniWindow.onClick(() => {
         console.log("Did click on miniwin cell");
-        here.exec(`ls ~/.Trash | wc -l`)
-            .then((stdOut) => {
-                const count = parseInt(stdOut);
-                console.log(`Files count: ${count}`);
-                if (typeof count == "number" && count == 0) {
-                    console.log(`The trash can is empty.`);
-                    return Promise.resolve();
-                } else {
-                    return here.exec("osascript -e 'tell application \"Finder\" to empty trash'");
-                }
-            })
-            .then((stdOut) => {
-                // console.log("stdOut: ", stdOut)
-                if (stdOut != undefined && stdOut.length > 0) {
-                    // Interactive command
-                    // Treat it as an Error for now
-                    console.error(`Error: ${stdOut}`);
-                    here.hudNotification("Failed to empty trash");
-                } else {
-                    here.hudNotification("Files deleted");
-                }
-            })
-            .catch((error) => {
-                console.error(`Faild to execute command. Error: ${JSON.stringify(error)}`);
-                here.hudNotification("Failed to empty trash");
-            });
+        onClick();
     });
     here.miniWindow.reload();
+
+    // Menu Bar
+    here.menuBar = new MenuBar();
+    here.menuBar.data = {
+        title: "Empty Trash",
+    };
+    here.menuBar.onClick(() => {
+        console.log(`on menu bar click.`)
+        onClick();
+    });
+    here.menuBar.reload();
 });
