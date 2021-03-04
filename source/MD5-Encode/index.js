a2aa66bf7de3c00a87ca5090f6173f32862b26ae
Here App Dev <HereDotApp@gmail.com>
2021-03-04 22:06:24 +0800

    feat(md5): addes here.on("query") support

--- source/MD5-Encode/index.js
+++ source/MD5-Encode/index.js
@@ -17,3 +17,19 @@ here.on("load", () => {
     });
     here.miniWindow.reload();
 });
+
+here.on("query", (query) => {
+    if (typeof query != "string") {
+        console.error(`query is not string!`);
+        return;
+    }
+    if (query.length == 0) {
+        console.warn(`query is empty.`)
+        return;
+    }
+
+    const encoded = crypto.md5(query);
+    console.log(`encoded: ${encoded}`)
+    pasteboard.setText(encoded);
+    here.postNotification("HUD", `String encoded: ${encoded}`);
+});
