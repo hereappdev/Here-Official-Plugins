cb4ab08437d7b8a8432ed0d2a4040d87889f8cd3
Here App Dev <HereDotApp@gmail.com>
2021-04-25 19:13:06 +0800

    refactor(weixin-host-post): refactor a little bit

--- source/WeXin-Hot-Post/index.js
+++ source/WeXin-Hot-Post/index.js
@@ -6,24 +6,22 @@ const pref = require("pref");
 // const jsonPref = pref.all()
 
 function getData(api) {
-    console.log("开始执行：getData()")
+    console.log(`getData ${api}`);
 
     const LIMIT = 30;
 
     let entryList = [];
     return http.get(api).then(function (response) {
-        console.log("执行：http.get(" + api + ")")
+        console.log(`response http.get(${api})`);
+        console.log(response);
         var json = [];
         // console.log(typeof(response.data.Data.data))
 
-        if (response == undefined
-            || response.data == undefined
-            || response.data.Data == undefined) {
-            console.error(`invalid response data`)
+        if (response == undefined || response.data == undefined || response.data.Data == undefined) {
+            console.error(`invalid response data`);
             here.miniWindow.set({ title: "Invalid data." });
-            return []
+            return [];
         }
-
 
         if (response.data.Data.data == undefined) {
             json = response.data.Data;
@@ -50,101 +48,75 @@ function getData(api) {
 function updateData() {
     here.miniWindow.data = { title: "Updating…" };
     here.miniWindow.reload();
-    Promise.all([
-        getData("https://api.tophub.fun/v2/GetAllInfoGzip?id=1104"),
-        getData("https://api.tophub.fun/v2/GetAllInfoGzip?id=1109"),
-        getData("https://api.tophub.fun/v2/GetAllInfoGzip?id=1132"),
-        getData("https://api.tophub.fun/v2/GetAllInfoGzip?id=1111"),
-        getData("https://api.tophub.fun/v2/GetAllInfoGzip?id=1129"),
-        getData("https://api.tophub.fun/v2/GetAllInfoGzip?id=1128"),
-        getData("https://api.tophub.fun/v2/GetAllInfoGzip?id=1134"),
-        getData("https://api.tophub.fun/v2/GetAllInfoGzip?id=1105"),
-        getData("https://api.tophub.fun/v2/GetAllInfoGzip?id=1107"),
-    ]).then(function (values) {
 
-        const topFeed = values[0][0];
-        if (topFeed == undefined) {
-            here.miniWindow.set({ title: "Invalid data." });
-            return
-        }
+    let sources = [
+        { name: "科技", url: "https://api.tophub.fun/v2/GetAllInfoGzip?id=1104" },
+        { name: "财经", url: "https://api.tophub.fun/v2/GetAllInfoGzip?id=1109" },
+        { name: "影视", url: "https://api.tophub.fun/v2/GetAllInfoGzip?id=1132" },
+        { name: "情感", url: "https://api.tophub.fun/v2/GetAllInfoGzip?id=1111" },
+        { name: "媒体", url: "https://api.tophub.fun/v2/GetAllInfoGzip?id=1129" },
+        { name: "政务", url: "https://api.tophub.fun/v2/GetAllInfoGzip?id=1128" },
+        { name: "健康", url: "https://api.tophub.fun/v2/GetAllInfoGzip?id=1134" },
+        { name: "搞笑", url: "https://api.tophub.fun/v2/GetAllInfoGzip?id=1105" },
+        { name: "开发", url: "https://api.tophub.fun/v2/GetAllInfoGzip?id=1107" },
+    ];
 
-        // Mini Window
-        here.miniWindow.data = {
-            title: topFeed.Title,
-            detail: "微信公众号热文",
-        };
-        here.miniWindow.onClick(function () {
-            here.openURL(topFeed.Url);
-        });
-        here.miniWindow.reload();
+    Promise.all(
+        sources.map((source) => {
+            return getData(source.url);
+        })
+    )
+        .then(function (values) {
+            const topFeed = values[0][0];
+            if (topFeed == undefined) {
+                here.miniWindow.set({ title: "Invalid data." });
+                return;
+            }
 
-        let popovers = [];
-
-        values.forEach(function (element, index) {
-            // console.log(index)
-            // console.log(values[index])
-
-            popovers[index] = _.map(values[index], (feed, index) => {
-                return {
-                    title: feed.Title.trim(),
-                    accessory: {
-                        title: feed.type.trim(),
-                    },
-                    // detail: feed.description,
-                    onClick: () => {
-                        here.openURL(feed.Url);
-                    },
-                };
+            // Mini Window
+            here.miniWindow.data = {
+                title: topFeed.Title,
+                detail: "微信公众号热文",
+            };
+            here.miniWindow.onClick(function () {
+                here.openURL(topFeed.Url);
             });
+            here.miniWindow.reload();
+
+            let popovers = [];
+
+            const tabs = values.map(function (value, index) {
+                // console.log(index)
+                // console.log(values[index])
+
+                // article list
+                const data =  _.map(value, (feed, index) => {
+                    return {
+                        title: feed.Title.trim(),
+                        accessory: {
+                            title: feed.type.trim(),
+                        },
+                        // detail: feed.description,
+                        onClick: () => {
+                            here.openURL(feed.Url);
+                        }
+                    };
+                });
+                return {
+                    title: sources[index].name,
+                    data: data
+                }
+            });
+            
+            // Popover
+            let popover = new TabPopover();
+            popover.data = tabs;
+            here.popover = popover;
+            here.popover.reload();
+        })
+        .catch(function (e) {
+            console.log(`catch error ${e}`);
         });
-
-        let tabs = [
-            {
-                title: "科技",
-                data: popovers[0],
-            },
-            {
-                title: "财经",
-                data: popovers[1],
-            },
-            {
-                title: "影视",
-                data: popovers[2],
-            },
-            {
-                title: "情感",
-                data: popovers[3],
-            },
-            {
-                title: "媒体",
-                data: popovers[4],
-            },
-            {
-                title: "政务",
-                data: popovers[5],
-            },
-            {
-                title: "健康",
-                data: popovers[6],
-            },
-            {
-                title: "搞笑",
-                data: popovers[7],
-            },
-            {
-                title: "开发",
-                data: popovers[8],
-            },
-        ];
-
-        // Popover
-        let popover = new TabPopover();
-        popover.data = tabs;
-        here.popover = popover;
-        here.popover.reload();
-    }).catch(function (e){
-        console.log(`catch error ${e}`);
-    });
 }
 
 here.on("load", () => {
